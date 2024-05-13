e440ace60110d4c5d3f688c24c3583f85a16bbc0
BRUNO ANDWELE ALVES ANTUNES <bruno.3a@outlook.com>
2024-05-13 18:54:32 -0300

    Update createSchemaAndTables.sql

--- createSchemaAndTables.sql
+++ createSchemaAndTables.sql
@@ -1,46 +1,6 @@
 CREATE SCHEMA faculdade
 
-CREATE TABLE faculdade. aluno(
-	Id_Aluno SERIAL PRIMARY KEY,
-	FOREIGN KEY (Id_Historico_Escolar) REFERENCES faculdade.historico_aluno,
-	FOREIGN KEY(Id_Curso) REFERENCES faculdade.curso,
-	Nome VARCHAR(30),
-	Sobrenome VARCHAR(60),
-	Data_Adesao DATE,
-	CPF VARCHAR(12),
-);
-
-CREATE TABLE faculdade. curso(
-	Id_Curso SERIAL PRIMARY KEY,
-	FOREIGN KEY(Id_Professor) REFERENCES faculdade.professor,
-	Titulo VARCHAR(30),
-);
-
-CREATE TABLE faculdade.historico_aluno(
-	Id_Historico_Escolar SERIAL PRIMARY KEY,
-	FOREIGN KEY(Id_Aluno) REFERENCES faculdade.aluno,
-	FOREIGN KEY(Id_Disciplina) REFERENCES faculdade.disciplina,
-	Media FLOAT(2),
-	Semestre VARCHAR(30),
-	Ano INT,
-);
-
-CREATE TABLE faculdade.grupo_aluno(
-	Id_Grupo_Aluno SERIAL PRIMARY KEY,
-	FOREIGN KEY (Id_Historico_TCC) REFERENCES faculdade.historico_tcc,
-	FOREIGN KEY(Id_Aluno) REFERENCES faculdade.aluno,
-);
-
-CREATE TABLE faculdade.historico_tcc(
-	Id_Historico_TCC SERIAL PRIMARY KEY,
-	FOREIGN KEY(Id_Grupo_Aluno) REFERENCES faculdade.grupo_aluno,
-	FOREIGN KEY(Id_Professor) REFERENCES faculdade.professor,
-	Semestre VARCHAR(30),
-	Ano INT,
-	Nota FLOAT(2),
-);
-
-CREATE TABLE faculdade.professor (
+CREATE TABLE faculdade.professor(
     Id_Professor SERIAL PRIMARY KEY,
     Nome VARCHAR(30),
 	Sobrenome VARCHAR(60),
@@ -48,10 +8,60 @@ CREATE TABLE faculdade.professor (
 	CPF VARCHAR(11)
 );
 
+CREATE TABLE faculdade.curso(
+	Id_Curso SERIAL PRIMARY KEY,
+	Id_Professor INT,
+	FOREIGN KEY(Id_Professor) REFERENCES faculdade.professor(Id_Professor),
+	Titulo VARCHAR(30)
+);
+
+CREATE TABLE faculdade.aluno(
+	Id_Aluno SERIAL PRIMARY KEY,
+	Id_Curso INT,
+	FOREIGN KEY(Id_Curso) REFERENCES faculdade.curso(Id_Curso),
+	Nome VARCHAR(30),
+	Sobrenome VARCHAR(60),
+	Data_Adesao DATE,
+	CPF VARCHAR(12)
+);
+
+
 CREATE TABLE faculdade.disciplina(
 	Id_Disciplina SERIAL PRIMARY KEY,
 	Titulo VARCHAR(30)	
 );
+
+
+CREATE TABLE faculdade.historico_aluno(
+	Id_Historico_Escolar SERIAL PRIMARY KEY,
+	Id_Aluno INT,
+	Id_Disciplina INT,
+	FOREIGN KEY(Id_Aluno) REFERENCES faculdade.aluno(Id_Aluno),
+	FOREIGN KEY(Id_Disciplina) REFERENCES faculdade.disciplina(Id_Disciplina),
+	Media FLOAT(2),
+	Semestre VARCHAR(30),
+	Ano INT
+);
+
+CREATE TABLE faculdade.grupo_aluno(
+	Id_Grupo_Aluno SERIAL PRIMARY KEY,
+	Id_Historico_TCC INT,
+	Id_Aluno INT,
+	FOREIGN KEY(Id_Aluno) REFERENCES faculdade.aluno(Id_Aluno)
+);
+
+
+CREATE TABLE faculdade.historico_tcc(
+	Id_Historico_TCC SERIAL PRIMARY KEY,
+	Id_Grupo_Aluno INT,
+	Id_Professor INT,
+	FOREIGN KEY(Id_Grupo_Aluno) REFERENCES faculdade.grupo_aluno(Id_Grupo_Aluno),
+	FOREIGN KEY(Id_Professor) REFERENCES faculdade.professor(Id_Professor),
+	Semestre VARCHAR(30),
+	Ano INT,
+	Nota FLOAT(2)
+);
+
 
 CREATE TABLE faculdade.departamento(
 	Id_Departamento SERIAL PRIMARY KEY,
